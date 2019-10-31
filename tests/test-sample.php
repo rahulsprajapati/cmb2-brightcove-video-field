10c64b35f2431d8774ba053d5227f945f3d7b483
Rahul Prajapati <rahul.prajapati@live.in>
2019-10-31 21:36:00 +0530

    Add phpunit scaffold generated files from wp cli.

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package cmb2-brightcove-video-field
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
